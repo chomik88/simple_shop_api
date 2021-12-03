4f8a2b62c6791fc1db17a468ed263beec60335f3
Rafał Kamka <rafal.kamka@ultrano.pl>
2021-12-03 11:00:59 +0100

    first commit

---
+++ routes/products.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product");
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:id", getProduct, (req, res) => {
+  res.json(res.product);
+});
+
+router.post("/", async (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    category: req.body.category,
+    thumbnail: req.body.thumbnail,
+  });
+
+  try {
+    const newProduct = await product.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.patch("/:id", getProduct, async (req, res) => {
+  if (req.body.name != null) {
+    res.product.name = req.body.name;
+  }
+  if (req.body.description != null) {
+    res.product.description = req.body.description;
+  }
+  if (req.body.category != null) {
+    res.product.category = req.body.category;
+  }
+  if (req.body.thumbnail != null) {
+    res.product.thumbnail = req.body.thumbnail;
+  }
+
+  try {
+    const updatedProduct = await res.product.save();
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:id", getProduct, async (req, res) => {
+  try {
+    await res.product.remove();
+    res.json({ message: `Deleted product with id: ${req.params.id}` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+async function getProduct(req, res, next) {
+  try {
+    product = await Product.findById(req.params.id);
+    if (product == null) {
+      return res.status(404).json({ message: "Cannot find product" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  res.product = product;
+  next();
+}
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to Mongo DB!");
+  })
+  .catch((error) => console.error("Connection problem:", error));
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const productsRouter = require('./routes/products');
+
+app.use('/products', productsRouter);
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));

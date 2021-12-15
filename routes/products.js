73b6bcce1c2b745dcc479b36231399242b9e00cc
Rafał Kamka <rafal.kamka@ultrano.pl>
2021-12-16 00:57:26 +0100

    Some changes

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/product");
 
+
 router.get("/", async (req, res) => {
   try {
     const products = await Product.find();

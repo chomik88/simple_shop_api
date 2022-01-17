4d1b0685cd9b3d1d09548a0c5e7aa136c03a1389
rkamka <rafal.kamka@deravesoftware.com>
2022-01-17 15:37:08 +0100

    morgan package added

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 const mongoose = require("mongoose");
 
 mongoose
